f26f69a681a9249275163c06ddfc5fba00067798
sergiskenderi1 <sergi.skenderi2017@gmail.com>
2019-01-26 17:40:35 +0100

    second commit

--- Test/src/testtest/Testing.java
+++ Test/src/testtest/Testing.java
@@ -5,4 +5,8 @@ public class Testing {
 	public static void main(String [] args) {
 		System.out.println("Pushing this local repo");
 	}
+	
+	public void test() {
+		System.out.println("2nd remote commit .");
+	}
 }
